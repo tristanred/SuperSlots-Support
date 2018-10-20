b730cd11683f6f1e8464fd44a423c161c1f09aab
Tristan Dubé <tristan.dube@gmail.com>
2018-10-20 12:59:51 -0400

    Started adding config file loading.
    Started with the reelstrip class, not done yet.
    
    Added pugiXML and libtech libraries as submodules. It is intended
    to switch the UnrealEngine project to load a built version of the
    support library. So this project will end up as a shared lib and
    be loaded from the SuperSlots project. This will avoid having
    to copy and paste the code from one project to another. Also since
    this project is now using dependencies of its own it is not viable
    to copy them all into the Unreal project.

--- src/ReelStrip.cpp
+++ src/ReelStrip.cpp
@@ -5,6 +5,10 @@
 
 #include "SymbolSet.h"
 
+#define PUGIXML_HEADER_ONLY
+
+#include "pugixml.hpp"
+
 ReelStrip::ReelStrip()
 {
     this->Length = 0;
@@ -20,6 +24,31 @@ ReelStrip::ReelStrip(Symbol** symbols, int length)
 ReelStrip::~ReelStrip()
 {
     delete[] this->Symbols;
+}
+
+ReelStrip* ReelStrip::GenerateReelstripFromConfig(const char* configPath, SymbolSet* symbols)
+{
+    pugi::xml_document doc;
+    pugi::xml_parse_result result = doc.load_file(configPath);
+    if(!result)
+    {
+        // File not found
+        return NULL;
+    }
+
+    // For now, read the reelstrip with id 0
+    pugi::xpath_node_set reelStripNodes = doc.select_nodes("/reelsetConfig/reelset[@id = '0']/reelstrip");
+
+    for(pugi::xpath_node node: reelStripNodes)
+    {
+        pugi::xml_node reelStrip = node.node();
+
+        const char* txt = reelStrip.text().as_string();
+
+
+    }
+
+    return NULL;
 }
 
 ReelStrip* ReelStrip::GenerateRandomReelstrip(int length, SymbolSet* symbols)
--- src/ReelStrip.h
+++ src/ReelStrip.h
@@ -13,5 +13,49 @@ public:
     int Length;
     Symbol** Symbols; // Borrowed list
 
+    static ReelStrip* GenerateReelstripFromConfig(const char* configPath, SymbolSet* symbols);
     static ReelStrip* GenerateRandomReelstrip(int length, SymbolSet* symbols);
+
 };
+
+/**
+ * Reelstrip file format
+ *
+ * reelstrip.conf
+ * <reelsetConfig>                  # Root node
+ *   <reelset id="0">               # Node for 1 set of reels
+ *      <reelstrip reel="0">
+ *          1, 2, 3, 4, 5, 6, 7, 8, 9, 0
+ *      </reelstrip>
+ *      <reelstrip reel="1">
+ *          1, 2, 3, 4, 5, 6, 7, 8, 9, 0
+ *      </reelstrip>
+ *      <reelstrip reel="2">
+ *          1, 2, 3, 4, 5, 6, 7, 8, 9, 0
+ *      </reelstrip>
+ *      <reelstrip reel="3">
+ *          1, 2, 3, 4, 5, 6, 7, 8, 9, 0
+ *      </reelstrip>
+ *      <reelstrip reel="4">
+ *          1, 2, 3, 4, 5, 6, 7, 8, 9, 0
+ *      </reelstrip>
+ *   </reelset>
+ *   <reelset id="1">
+ *      <reelstrip reel="0">
+ *          1, 2, 3, 4, 5, 6, 7, 8, 9, 0
+ *      </reelstrip>
+ *      <reelstrip reel="1">
+ *          1, 2, 3, 4, 5, 6, 7, 8, 9, 0
+ *      </reelstrip>
+ *      <reelstrip reel="2">
+ *          1, 2, 3, 4, 5, 6, 7, 8, 9, 0
+ *      </reelstrip>
+ *      <reelstrip reel="3">
+ *          1, 2, 3, 4, 5, 6, 7, 8, 9, 0
+ *      </reelstrip>
+ *      <reelstrip reel="4">
+ *          1, 2, 3, 4, 5, 6, 7, 8, 9, 0
+ *      </reelstrip>
+ *   </reelset>
+ * </reelsetConfig>
+ */
